86dece744be7607c7fc4ffd8c22bd9f578f4661c
hottomo0610 <t.hotta@withpassion.co.jp>
2022-03-08 16:43:37 +0900

    Update Utility.php

--- src/SlackAPIUtil/Utility.php
+++ src/SlackAPIUtil/Utility.php
@@ -22,36 +22,30 @@ class Utility
      * 
      * @return
      */
-    public function getChannelList(string $token) 
+    public function getChannelList($token) 
     {
         //SlackのWPワークスペース内にあるチャンネルの情報を取得
 
-       $ch = curl_init();
-       curl_setopt_array($ch, [
-           CURLOPT_RETURNTRANSFER => 1,
-           CURLOPT_URL => "https://slack.com/api/conversations.list?token=$token&exclude_archived=true&limit=500&types=public_channel"
-       ]);
-       $resp = curl_exec($ch);
-       curl_close($ch);
+        $post_data = [
+            'token' => $token
+        ];
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_data));
+        curl_setopt_array($ch, [
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => "https://slack.com/api/conversations.list?exclude_archived=true&limit=500&types=public_channel"
+        ]);
+        $resp = curl_exec($ch);
+        curl_close($ch);
     
-       $decodedData = json_decode($resp, true);
-       $channel_list = $decodedData['channels'];
+        $decodedData = json_decode($resp, true);
+        $channel_list = $decodedData['channels'];
     
-    
-       // 名前に特定の文字列が含まれるチャンネルを除外する場合の処理
-       //
-       // $i = 0;
-       // foreach($channel_list as $key){
-       //     if(strpos($key['name'], 'times') !== false){
-       //         unset($channel_list[$i]);
-       //     }
-       //     $i++;
-       // }
-    
-       $channel_list = array_values($channel_list);
-    
-       //error_log(print_r($channel_list, true));
-       return $channel_list;
+        $channel_list = array_values($channel_list);
+
+        return $channel_list;
     }
 
 
@@ -64,10 +58,16 @@ class Utility
      */
     public function getMessage(string $token, string $channelId) 
     {
+        $post_data = [
+            'token' => $token
+        ];
+
         $ch = curl_init();
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_data));
         curl_setopt_array($ch, [
             CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_URL => "https://slack.com/api/conversations.history?token=$token&channel=$channelId&inclusive=true"
+            CURLOPT_URL => "https://slack.com/api/conversations.history?channel=$channelId&inclusive=true"
         ]);
         $resp = curl_exec($ch);
         curl_close($ch);
@@ -87,21 +87,27 @@ class Utility
      */
     public function getMessageInPeriod(string $token, string $channelId, DateTime $before_time, DateTime $near_time) 
     {
-         //Slackの特定のチャンネルから、今日から〇日前までのメッセージを取得
+        //Slackの特定のチャンネルから、今日から〇日前までのメッセージを取得
 
-         $before_time_U = $before_time -> format('U');
-         $near_time_U = $near_time -> format('U');
+        $post_data = [
+            'token' => $token
+        ];
 
-         $ch = curl_init();
-         curl_setopt_array($ch, [
-             CURLOPT_RETURNTRANSFER => 1,
-             CURLOPT_URL => "https://slack.com/api/conversations.history?token=$token&channel=$channelId&inclusive=true&oldest=$before_time_U&latest=$near_time_U"
-         ]);
-         $resp = curl_exec($ch);
-         curl_close($ch);
+        $before_time_U = $before_time -> format('U');
+        $near_time_U = $near_time -> format('U');
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_data));
+        curl_setopt_array($ch, [
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => "https://slack.com/api/conversations.history?token=$token&channel=$channelId&inclusive=true&oldest=$before_time_U&latest=$near_time_U"
+        ]);
+        $resp = curl_exec($ch);
+        curl_close($ch);
  
-         $decodedData = json_decode($resp, true);
-         return $decodedData;
+        $decodedData = json_decode($resp, true);
+        return $decodedData;
     }
 
 
